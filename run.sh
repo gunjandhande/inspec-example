1f862fbb3e6117ccb1052fa8177cc39b769b9597
gunjandhande <43988913+gunjandhande@users.noreply.github.com>
2020-06-02 10:48:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 bundle install &&
-inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format html >> report/results.html
-inspec exec inspec-workshop -t winrm://${SPEC_USER}@${SPEC_HOST_NAME} --password "${SPEC_PASSWORD}" --format json-rspec >> report/results.json
+inspec exec . -t winrm://${INSPEC_USER}@${INSPEC_IP} --password "${INSPEC_PASSWORD}" --format html >> reports/results.html
+inspec exec . -t winrm://${INSPEC_USER}@${INSPEC_IP} --password "${INSPEC_PASSWORD}" --format json-rspec >> reports/results.json
 
